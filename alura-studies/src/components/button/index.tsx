9978613810bf9c8a1a26615be375eab0d8be6de8
Danielle Sousa <daniellexsousa@gmail.com>
2022-02-22 11:40:38 -0300

    criação de estrutura inicial e componentes

---
+++ alura-studies/src/components/button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button>Play</button>
+        )
+    }
+}
+
+export default Button;
---
+++ alura-studies/src/components/list/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function List() {
+    return (
+        <aside>
+            <h2>Estudos do dia</h2>
+
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>01:30:00</span>
+                </li>
+
+                <li>
+                    <h3>Javascript</h3>
+                    <span>00:45:00</span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
